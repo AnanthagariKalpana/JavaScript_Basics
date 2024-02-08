08b99c986e5f3a78116b706373bb59759e5e12b0
kalpana <kalpanas1929@gmail.com>
2024-02-08 10:52:50 +0530

    addedhosting

--- Hosting/hosting.js
+++ Hosting/hosting.js
@@ -1,4 +1,8 @@
 //before intialize we are printing the variable a
-console.log(a);//undefined
-var a=10;
-console.log(a);//10
+// console.log(a);//undefined
+// var a=10;
+// console.log(a);//10
+
+console.log(l);// Error: Cannot access 'l' before initialization
+let l=5;
+console.log(l);
