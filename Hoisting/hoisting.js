15ee537d980b76a97b940d8286c55a00e0de186a
kalpana <kalpanas1929@gmail.com>
2024-02-08 11:07:43 +0530

    addedhoisting

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,16 @@
+//before intialize we are printing the variable a
+// console.log(a);//undefined
+// var a=10;
+// console.log(a);//10
+
+// console.log(l);// Error: Cannot access 'l' before initialization
+// let l=5;
+// console.log(l);
+
+// console.log(c);//Error: Cannot access 'c' before initialization
+// const c=true;
+// console.log(c);
+
+x=1;
+alert("x="+x);
+var x;
--- Hosting/hosting.js
+++
@@ -1,8 +0,0 @@
-//before intialize we are printing the variable a
-// console.log(a);//undefined
-// var a=10;
-// console.log(a);//10
-
-console.log(l);// Error: Cannot access 'l' before initialization
-let l=5;
-console.log(l);
