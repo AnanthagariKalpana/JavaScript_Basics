2f99ea030eb6ac72fbab01f338c27ffc02c4515b
kalpana <kalpanas1929@gmail.com>
2024-02-08 12:50:56 +0530

    addedAwait

--- CallBackFunction/await.js
+++ CallBackFunction/await.js
@@ -1,19 +1,41 @@
-function abc()
+// function abc()
+// {
+//     console.log("abc function")
+// }
+
+// const xyz=()=>{
+//     return new Promise((resolve,reject)=>{
+//         let value=false
+//         if(value===true)
+//         {
+//             resolve(abc());
+//         }
+//         else{
+//             reject("promises rejected");
+//         }
+//     })
+// }
+// xyz().then((result)=>console.log("after resolve.......",result))
+//     .catch((error)=>console.log("After rejected.........",error))
+
+function add(a,b)
 {
-    console.log("abc function")
+    console.log(a+b);
 }
 
-const xyz=()=>{
-    return new Promise((resolve,reject)=>{
-        let value=false
-        if(value===true)
+let addOpe=()=>{
+    return new Promise((resolve, reject) => {
+        let a=6;
+        let b=6;
+        if(a == b)
         {
-            resolve(abc());
+            add(a,b)
+            resolve("promises full fill")
         }
         else{
-            reject("promises rejected");
+            reject("A and B are not equal")
         }
-    })
-}
-xyz().then((result)=>console.log("after resolve.......",result))
-    .catch((error)=>console.log("After rejected.........",error))
+    });
+};
+addOpe().then((res)=>console.log(res,"\nA and B are equal........"))
+    .catch((error)=>console.log(error,"\nPromises rejected......."))
