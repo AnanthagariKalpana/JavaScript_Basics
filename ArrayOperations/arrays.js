ac151c682822a602c0ceeb009e417a8d32b9ae96
kalpana <kalpanas1929@gmail.com>
2024-02-08 15:49:16 +0530

    arrayAdded

--- ArrayOperations/arrays.js
+++ ArrayOperations/arrays.js
@@ -1,3 +1,4 @@
+//1........Creating an array and inserting one by one into array
 const arr=[];
 arr[0]=1;
 arr[1]=2;
@@ -10,3 +11,17 @@ console.log(arr[1]);
 console.log(arr[2]);
 console.log(arr[3]);
 console.log(arr[4]);
+
+//2...........creating an and using for loop to print
+const arr1=new Array()
+arr1[0]="mango";
+arr1[1]="apple";
+arr1[2]="banana";
+for(let i=0;i<arr1.length;i++)
+{
+    console.log(arr1[i]+"")
+}
+//3..............
+const std={name:"kalpana",age:23};
+console.log("Name: ",std.name);
+console.log("Age: "+std.age);
