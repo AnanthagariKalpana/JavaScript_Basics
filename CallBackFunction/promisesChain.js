2a12ce85b706759b86517ef341e8e8ff154d37e0
kalpana <kalpanas1929@gmail.com>
2024-02-08 15:01:31 +0530

    updated promises chaining

--- CallBackFunction/promisesChain.js
+++ CallBackFunction/promisesChain.js
@@ -1,3 +1,5 @@
+//1........promisesChaining : fecting the data and again updating the same data
+
 // function fetchdata()
 // {
 //    return new Promise((resolve, reject) => {
@@ -30,6 +32,8 @@
 // .catch((error)=>console.log("promises failed",error));
 
 
+
+//2..........fecting the st1 and continuing the promises to fecth the st2...
 function std()
 {
     return new Promise((resolve, reject) => {
@@ -57,4 +61,33 @@ std().then(res=>
     .then(upres=>console.log("student2 is added..",upres))
     .then(final=>console.log("promises chaining completed..",final))
     .catch(error=>console.log("promises filed...",error))
+
+
+
+
+
+//3..........firstpromises is adding the ADD operation && continue the promises to do SUB operation
+function add()
+{
+    return new Promise((resolve, reject) => {
+        let a=10;
+        let b=5;
+        resolve(a+b);
+    },1500);
+}
+function sub()
+{
+    return new Promise((resolve, reject) => {
+        let a=15;
+        let b=5;
+        resolve(a-b);
+    },1000);
+}
+
+add().then(res=>{
+    console.log("Addition result is: ",res);
+    return sub();
+})
+.then(res1=>console.log("Subtraction result is: ",res1))
+.catch(error=>console.log("promises rejected...",error))
   
