db04d93812ef6e4f9d252d9760dcc214bbd5f6da
kalpana <kalpanas1929@gmail.com>
2024-02-09 10:17:41 +0530

    objectsadd

---
+++ Objects/object.js
@@ -0,0 +1,18 @@
+const student=
+{
+    name:'Mohan',
+    age: 30
+};
+console.log("Name: " ,student.name);
+console.log("Age: ",student.age);
+
+const person=
+{
+    name:'kalpana',
+    age: 23,
+    profession :'developer'
+}
+console.log("Name: " ,person.name);
+console.log("Age: ",person.age);
+console.log("Profession: ",person.profession)
+
