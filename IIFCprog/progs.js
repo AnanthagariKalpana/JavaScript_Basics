15eb11a895f2174471aff248d148d2481a36ecab
kalpana <kalpanas1929@gmail.com>
2024-02-09 15:27:39 +0530

    iifcfunction

---
+++ IIFCprog/progs.js
@@ -0,0 +1,33 @@
+(function(){
+ var x=10;
+ console.log(x);
+})();
+
+(function(name){
+    console.log("hello",name);
+}
+)("maha");
+
+let a1=(function(a,b){
+    return (a+b);
+})(10,10);
+console.log(a1);
+
+var counter=(function(){
+    var count=0;
+    return {
+        increment: function(){
+            count++;
+        },
+        decrement: function()
+        {
+            count--;
+        },getCount:function()
+        {
+            return count;
+        }
+    }
+})();
+counter.increment();
+counter.decrement();
+console.log(counter.getCount());
